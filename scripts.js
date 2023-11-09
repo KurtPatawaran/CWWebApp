7050986c2841ff87312cbd7137f99940e2203312
Kurt Patawaran <kurtxsp@gmail.com>
2023-11-09 18:56:40 +0400

    Coded 20% of the parts

--- scripts.js
+++ scripts.js
@@ -4,6 +4,15 @@ let webstore = new Vue({
     data:
     {
         siteName: 'After School Activities',
+
+        subject:{
+            title: "Math",
+            location: "Dubai",
+            image: "mathicon.png",
+            price: 350 + "AED",
+            space:10,               //User/Student View
+            availableSpaces:10      //Inventory View
+        }
     },
 
     methods:
