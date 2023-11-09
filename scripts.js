082491628febeea988b7e87bff7f1f8a1345b834
Kurt Patawaran <kurtxsp@gmail.com>
2023-11-09 21:55:22 +0400

    Fixed AddToCart fucntion

--- scripts.js
+++ scripts.js
@@ -4,33 +4,51 @@ let webstore = new Vue({
     data:
     {
         siteName: 'After School Activities',
+        showSubject: true,
+
+        order: {
+            firstName: '',
+            lastName: '',
+            contactNum: '',
+            
+        },
 
         subject:{
+            id: 1001,
             title: "Math",
             location: "Dubai",
-            image: "mathicon.png",
+            image: "mathIcon.png",
             price: 350 + "AED",
-            space:10,               //User/Student View
-            availableSpaces:10      //Inventory View
-        }
+            space:10,               //User Student View
+            availableSpaces:10,      //Inventory View
+        },
+        cart:[],
     },
 
     methods:
     {
+        addToCart:function () {
+            this.cart.push(this.subject.id);    //console.log(this.cart)
+            this.subject.space--;
+        },
 
+        showCheckout(){
+            this.showSubject = !this.showSubject;
+        },
+
+        submitForm() {
+            {alert('You Have Succesfully Applied :D')}
+        }
     },
 
     computed:
     {
-        cartItemCount:  function() {
-            return this.cartItemCount.length || '';
-        },
+        // cartItemCount:  function() {
+        //     return this.cartItemCount.length || '';
+        // },
 
         canAddToCart:   function() {
-            return this.product.availableSpaces > this.cartItemCount;
-        }
-    }
-
-
-
+            return this.subject.availableSpaces > this.cart.length;
+        },
+    },
 });
