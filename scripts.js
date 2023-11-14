36dc19b481d3769a84df05bec6607a6a693d8e76
Kurt Patawaran <kurtxsp@gmail.com>
2023-11-14 20:08:34 +0400

    Updates but errors are showing

--- scripts.js
+++ scripts.js
@@ -1,63 +1,85 @@
 let webstore = new Vue({
-    el:'#app',
+    el: '#app',
 
     data:
     {
         siteName: 'After School Activities',
         showSubject: true,
-        
+
         order: {
             firstName: '',
             lastName: '',
             contactNum: '',
-            
+
         },
 
-        // subject:{
-        //     id: 1001,
-        //     title: "Math",
-        //     location: "Dubai",
-        //     image: "mathIcon.png",
-        //     price: 350 + "AED",
-        //     space:10,               //User Student View
-        //     availableSpaces:10,      //Inventory View
-        // },
 
-        subject: subject,
-        cart:[],
+        subjects: subjects,
+        cart: [],
     },
 
     methods:
     {
-        addToCart:function () {
-            this.cart.push(this.subject.id);    //console.log(this.cart)
+        addToCart: function (subject) {
+            this.cart.push(subject.id);    //console.log(this.cart)
             this.subject.space--;
         },
 
-        showCheckout(){ //If cart is empty, Prompt the user to choose a subject
+
+
+        showCheckout() { //If cart is empty, Prompt the user to choose a subject
             // if (this.cart.length > 0) {
-                this.showSubject = !this.showSubject;
+            this.showSubject = !this.showSubject;
             // } else {
             //     alert('Add an item to the cart to proceed to checkout.');
             // }
         },
 
         submitForm() {
-            {alert('You Have Succesfully Applied :D')}
-        }
+            { alert('You Have Succesfully Applied :D') }
+        },
+
+        cartCount(id) {
+            let count = 0;
+            for (let i = 0; i < this.cart.length; i++) {
+                if (this.cart[i] === id) {
+                    count++
+                }
+            }
+            return count;
+        },
+
+        // getSubjects() {
+        //     if (typeof subjects !== "undefined") {
+        //         this.subject = subjects;
+        //     }
+        // },
+
     },
 
     computed:
     {
         cartItemCount: function () {
-            return this.cart.length || 0; //This determines the number in the cart . Used in codnition such as Buy Now! in subject spaces
+            return this.cart.length || ''; //This determines the number in the cart . Used in codnition such as Buy Now! in subject spaces
         },
 
-        canAddToCart:   function() {
-            return this.subject.availableSpaces > this.cart.length; //THis condition check if theres more available spaces than the cart, If true user can add to the cart
-        },
+        // canAddToCart:   function() {
+        //     return this.subject.availableSpaces > this.cart.length; //THis condition check if theres more available spaces than the cart, If true user can add to the cart
+        // },
 
+        sortedSubjects() {
+            let subjectsArray = this.subjects.slice(0);
+            function compare(a, b) {
+                if (a.price > b.price)
+                    return 1;
+                if (a.price < b.price)
+                    return -1;
+                return 0;
+            }
+            return subjectArray.sort(compare);
+        }
 
     },
-});
 
+
+});
